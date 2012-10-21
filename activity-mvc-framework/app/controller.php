5031b63cc6c1f8806616ad95823561f5dc6908e1
Gaurish Sharma <contact@gaurishsharma.com>
2012-10-21 20:50:18 +0530

    Framework works now, pages can be switched

--- activity-mvc-framework/app/controller.php
+++ activity-mvc-framework/app/controller.php
@@ -3,6 +3,7 @@
 class Controller {
     public $load;
     public $model;
+    public $current_page;
     
     function __construct(){
         $this->load = new Load();
@@ -10,24 +11,38 @@ class Controller {
     }
     
     function router() {
-        switch(isset($_GET['page'])) {
-            case 'contact':
-                $this->display('contact.php');
-                break;
-            case 'gallery':
-                $this->display('gallery.php');
-                break;
-            case 'fixture_info':
-                $this->display('fixture_info.php');
-                break;
-            default:
-                $this->display('home.php');
-                break;
+        $this->current_page = $_GET['page'];
+
+        if( $this->current_page == 'contact') {
+                $this->contact();
+        } elseif( $this->current_page == 'gallery') {
+                $this->gallery();
+        } elseif( $this->current_page == 'fixture_info') {
+                $this->fixture_info();
+        } else {
+            $this->home();
         }
+
+   }
+   
+   function contact(){
+        $this->display('contact.php');
+   }
+
+   function gallery(){
+        $this->display('gallery.php');
+   }
+
+   function fixture_info(){
+        $this->display('fixture_info.php');
+   }
+
+   function home(){
+        $this->display('home.php');
+
    }
 
    function display($page){
-        $this->load->view('header.php');
         $this->load->view($page);
    }
 }
